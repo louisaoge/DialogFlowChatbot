bf8e38c17d1fcaa443c1c1acdd134a66ce413b6c
Louisa Okwuone <louisaoge@gmail.com>
2019-02-07 13:48:10 +0100

    minor change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var port = process.env.PORT || 8080;
 const server = express();
 server.use(bodyParser.json());
 server.post('/getMovies',function (request,response)  {
-    if(request.body.result.parameters['top-rated']) {
+    if(request.queryResult.parameters = "top-rated") {
         var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
             req.query({
                 "page": "1",

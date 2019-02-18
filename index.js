3082730fe33bed01123bb785a5e52a9df4e4b785
Louisa Okwuone <louisaoge@gmail.com>
2019-02-18 11:20:28 +0100

    new endpoints

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('https');
 var unirest = require("unirest");
+var request = require("request");
+const url_1 ="https://api.themoviedb.org/3/movie/";
+const url_2="http://34.242.179.194:8080/ords/api-v2.0/mobile/rest/";
+const url_3 = "https://u259.neuappx.com/ords/api-v2.0/mobile/rest/";
 let errorResposne = {
     results: []
 };
@@ -13,7 +17,7 @@ server.use(bodyParser.json());
 server.post('/getMovies',function (request,response)  {
     if(request.body.queryResult.parameters['top-rated']) {
        // console.log(request.body.queryResult.parameters['top-rated']);
-        var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
+        var req = unirest("GET", url_1+"top_rated");
             req.query({
                 "page": "1",
                 "language": "en-US",
@@ -81,45 +85,66 @@ server.post('/getMovies',function (request,response)  {
                 }
             });
 
-    }  else if(request.body.queryResult.parameters['m_funds']) {
-        //   console.log('popular-movies param found');
-          // let movie = request.body.queryResult.queryText;
-           var req = unirest("GET", "http://34.242.179.194:8080/ords/api-v2.0/mobile/rest/GetMBFunds/");
-             //  req.query({
-               //    "include_adult": "true",
-               //    "page": "1",
-               //    "query":movie,
-                 //  "language": "en-US",
-                  // "api_key": "d040a6cd3182427fd1b5e05967e143e8"
-             // });
-               req.send("{}");
-               req.end(function(res) {
-                   if(res.error) {
-                       response.setHeader('Content-Type', 'application/json');
-                       response.send(JSON.stringify({
-                           "fulfillmentText" : res.error,
-                           "fulfillmentMessages" : [{"text": {"text": [JSON.parse(JSON.stringify(res))]}}],
-                           "source": "Text response"
-                       }));
-                   }else if(res.body.result.length > 0) {
-                   // let result = res.body.result[0].GetFunds;
-                    let output = '';
-                   // for(let i = 0; i<result.length;i++) {
-                      //  output += result[i].FUND_DESCRIPTION;
-                       // output+="\n"
-                       output="hiiii";
-                    
-                   } response.setHeader('Content-Type', 'application/json');
-                   response.send(JSON.stringify({
-                       "fulfillmentText" : output,
-                       "fulfillmentMessages" :[{"text": {"text": [output]}}],
-                       "source":""
-                   })); 
-               })
-           }
-        
-    
-    else if(request.body.queryResult.parameters['popular-movies']) {    
+    } 
+    else if (request.body.queryResult.parameters['m_funds']){
+
+var request = unirest.get(url_2+'GetMBFunds/');
+request.send({});
+request.end(function(res) {
+    if(res.error) {
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify({
+            "fulfillmentText" : res,
+            "fulfillmentMessages" : [{"text": {"text": ["Error. Can you try it again ? "]}}]
+        }));
+    } else if(res.body.result.length > 0) {
+        let result = res.body.result[0].GetFunds;
+        let output = '';
+        for(let i = 0; i<result.length;i++) {
+            output += result[i].FUND_DESCRIPTION;
+            output+=" \n "
+        }
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify({
+            "fulfillmentText" : output,
+            "fulfillmentMessages" :[{"text": {"text": [output]}}],
+            "source":""
+        })); 
+    }
+})
+
+    }
+    else if(request.body.queryResult.parameters['Get_fund_price']) {
+        // console.log(request.body.queryResult.parameters['top-rated']);
+         var req = unirest("GET",url_2+"GetMBFundPrice/"+request.body.queryResult.parameters.Get_fund_price+"/");
+             //req.query({
+               //  "page": "1",
+                // "language": "en-US",
+               //  "api_key": "d040a6cd3182427fd1b5e05967e143e8"
+           //  });
+             req.send({});
+             req.end(function(res) {
+                 if(res.error) {
+                     response.setHeader('Content-Type', 'application/json');
+                     response.send(JSON.stringify({
+                         "fulfillmentText" : "Error. Can you try it again ? ",
+                         "fulfillmentMessages" : [{"text": {"text": ["Error. Can you try it again ? "]}}]
+                     }));
+                 } else if(res.body.result.length > 0) {
+                     let result = res.body.result[0].GetFundPrice;
+                     let output = 'Bid Price ='+result[0].BID_PRICE + "\n Offer Price : " + result[0].OFFER_PRICE;
+                     
+                     response.setHeader('Content-Type', 'application/json');
+                     response.send(JSON.stringify({
+                         "fulfillmentText" : output,
+                         "fulfillmentMessages" :[{"text": {"text": [output]}}],
+                         "source":""
+                     })); 
+                 }
+             });
+     }
+
+     else if(request.body.queryResult.parameters['popular-movies']) {    
         var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
             req.query({
                 "page": "1",
@@ -150,9 +175,12 @@ server.post('/getMovies',function (request,response)  {
                 }
             });
     }
+
 });
 server.get('/getName',function (req,res){
-    res.send('Louisa');
+    res.send(JSON.stringify({ "fulfillmentText" : "output",
+    "fulfillmentMessages" :[{"text": {"text": ["output"]}}],
+    "source":""}));
 });
 server.listen(port, function () {
     console.log("Server is up and running...");
